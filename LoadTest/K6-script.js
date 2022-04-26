5a13e3aa3f25bfd0ffd1c772700729eb250243ec
Noviadi Suryo <noviadi@wartek.belajar.id>
2022-04-27 01:39:35 +0700

    delete treshold script k6 local

--- LoadTest/K6-script.js
+++ LoadTest/K6-script.js
@@ -2,19 +2,19 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
-export const options = {
-  stages: [
-    { duration: '30s', target: 20 },
-    { duration: '1m30s', target: 10 },
-    { duration: '20s', target: 0 },
-  ],
-
-  thresholds: {
-    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-    http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
-  },
-  
-};
+//export const options = {
+//  stages: [
+//    { duration: '30s', target: 20 },
+//    { duration: '1m30s', target: 10 },
+//    { duration: '20s', target: 0 },
+//  ],
+//
+//  thresholds: {
+//    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+//    http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
+//  },
+//
+//};
 
 export default function () {
   let res = http.get('https://test.k6.io');
