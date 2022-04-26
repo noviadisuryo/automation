d663e9854d83c5427293b9e54736731d019e78b7
Noviadi Suryo <noviadi@wartek.belajar.id>
2022-04-26 23:08:52 +0700

    add k6

---
+++ LoadTest/K6-script.js
@@ -0,0 +1,25 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '30s', target: 20 },
+    { duration: '1m30s', target: 10 },
+    { duration: '20s', target: 0 },
+  ],
+
+  thresholds: {
+    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
+  },
+  
+};
+
+export default function () {
+  let res = http.get('https://test.k6.io');
+  check(res, {
+    'is status 200': (r) => r.status === 200,
+    'text verification': (r) => r.body.includes("Collection of simple web-pages suitable for load testing"),
+  });
+  sleep(1);
+}
