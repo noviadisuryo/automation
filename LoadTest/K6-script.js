189a00e07e0890474fcc0049a017dced7612d26c
Noviadi Suryo <noviadi@wartek.belajar.id>
2022-04-27 01:13:35 +0700

    add reporter to k6 local file

--- LoadTest/K6-script.js
+++ LoadTest/K6-script.js
@@ -1,5 +1,6 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export const options = {
   stages: [
@@ -22,4 +23,11 @@ export default function () {
     'text verification': (r) => r.body.includes("Collection of simple web-pages suitable for load testing"),
   });
   sleep(1);
+}
+
+export function handleSummary(data) {
+  return {
+    "new2.html": htmlReport(data), // show report in html based format.
+    'stdout': textSummary(data, { indent: ' ', enableColors: true }), // Show the text summary to stdout format...
+  };
 }
