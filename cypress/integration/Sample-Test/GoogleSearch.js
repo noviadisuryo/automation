8b70e0ece7d08ffb772e26a2d935ec03fb3bbd37
Noviadi Suryo <noviadi@wartek.belajar.id>
2022-04-28 14:32:52 +0700

    edit cypress test trigger path

--- cypress/integration/Sample-Test/GoogleSearch.js
+++ cypress/integration/Sample-Test/GoogleSearch.js
@@ -6,9 +6,15 @@ describe('Cypress UI Automation Sample', () => {
         cy.visit('https://www.google.com/')
 
         // Wait Page Load
-        cy.wait(3000)
-
-        cy.get('input[name="q"]').type('QA Engineer{enter}');
+        cy.wait(2000)
+        // Enter "QA Engineer" query & Tap ENTER
+        cy.get('input[name="q"]').type('QA Engineer{enter}')
         cy.contains('QA Engineer').should('be.visible')
+        // Click on Image TAB
+        cy.get('.MUFPAc > :nth-child(2) > a').click()
+        // Click MORE Tab
+        cy.get('.rIbAWc > .NZmxZe').click()
+        // Click MAP
+        cy.get('.ErsxPb-AHUcCb > .SiT4cd-ibnC6b-bN97Pc > a').click()
    })
 })
